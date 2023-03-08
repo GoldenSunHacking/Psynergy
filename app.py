8a74e95be22355974f50b228be4cd26e740dc6c0
Mia Moretti <mimickal.dev@gmail.com>
2023-03-07 19:21:22 -0500

    Hilarious hack to make ctrl+c work again

--- app.py
+++ app.py
@@ -3,10 +3,24 @@ from views.main_window import MainWindow
 
 # TODO we will probably want to create a state object we can read from for the UI.
 # Saved config, themes, window locations, etc... go here
-def startApp():
-    app = QApplication([])
 
-    mainWindow = MainWindow()
-    mainWindow.show()
+class MagicApp(QApplication):
+    def exec(self):
+        mainWindow = MainWindow()
+        mainWindow.show()
 
-    return app.exec()
+        # See event() below.
+        self.startTimer(200)
+
+        super().exec()
+
+    # Allows exiting via interrupt (ctrl+c).
+    #
+    # By default, Python spends all of its time in native Qt land, meaning it
+    # never gets a chance to handle our Python-level interrupt events.
+    # This, combined with an app timer and a signal handler, occasionally makes
+    # Qt give Python back control, so it can handle our interupts.
+    #
+    # See: https://stackoverflow.com/a/11705366/7954860
+    def event(self, e):
+        return super().event(e)
--- cli.py
+++ cli.py
@@ -1,7 +1,18 @@
-import sys
-from app import startApp
+from signal import signal, SIGINT
+from sys import exit
+
+from app import MagicApp
 
 # This is where things like CLI argument handling goes
 
+# TODO argv handling?
+app = MagicApp([])
+
+def handleInterrupt(sig, frame):
+    print('Unclean exit via interrupt!')
+    app.quit()
+    exit()
+
 if __name__ == '__main__':
-    sys.exit(startApp())
+    signal(SIGINT, handleInterrupt)
+    exit(app.exec())
