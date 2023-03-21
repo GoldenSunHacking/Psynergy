654707de913c1bc91919b167f6497d9ab347a183
Mia Moretti <mimickal.dev@gmail.com>
2023-03-21 01:00:34 -0400

    Override PROGRAM_VERSION in local development

--- info.py
+++ info.py
@@ -1,3 +1,12 @@
+import logging
+import gitinfo
+
 PROGRAM_NAME = 'Psynergy'
 PROGRAM_DESCRIPTION='Graphical editor for Golden Sun and other Camelot games.'
-PROGRAM_VERSION = '0.0.0-dev'
+PROGRAM_VERSION = '0.0.0'
+
+# Override PROGRAM_VERSION for local development
+logging.getLogger(gitinfo.__name__).setLevel(logging.CRITICAL)
+commit = gitinfo.get_git_info()
+if commit is not None:
+    PROGRAM_VERSION = f"dev {commit.get('author_date')} ({commit.get('commit')})"
