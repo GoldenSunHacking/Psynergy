ac4309bf8b0d524e378e914c324ac5e5f1217329
Mia Moretti <mimickal.dev@gmail.com>
2023-03-24 06:58:03 -0400

    Allow string editor to disallow invalid characters

--- views/text_editor.py
+++ views/text_editor.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import (
     QGroupBox,
     QHBoxLayout,
@@ -50,14 +51,17 @@ class TextEditTab(QGroupBox):
         stringList = StringList(items)
         return stringList
 
-    def makeEditBox(self) -> QTextEdit:
-        editBox = QTextEdit()
-        editBox.setPlaceholderText('Select string to edit')
+    def makeEditBox(self) -> 'EditBox':
+        editBox = EditBox()
+        # TODO apply real whitelist for loaded game
+        editBox.setWhitelist('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
+        editBox.validationFailure.connect(lambda msg: print(msg))
         return editBox
 
     def makePreviewBox(self) -> QTextEdit:
         # TODO make this look like a text box from the game, with the proper font.
         previewBox = QTextEdit()
+        previewBox.setDisabled(True)
         previewBox.setReadOnly(True)
         previewBox.setPlaceholderText('In-game preview')
         return previewBox
@@ -76,3 +80,48 @@ class TextEditTab(QGroupBox):
             self.previewBox.setText(self.editBox.toPlainText())
         self.editBox.textChanged.connect(setPreview)
 
+class EditBox(QTextEdit):
+    '''A control for editing a game string.
+
+    Entered text can be restricted to a valid character range for the game.
+    '''
+
+    validationFailure = pyqtSignal(str)
+    'Signal for when an invalid character is entered.'
+
+    def __init__(self):
+        super().__init__()
+        self.setAcceptRichText(False)
+        self.setDisabled(True)
+        self.setPlaceholderText('Select string to edit')
+
+        self._charWhitelist: Optional[str] = None
+        self._lastGoodText: Optional[str] = None
+
+        self.textChanged.connect(self._clearInvalidChars)
+
+    def setText(self, text: str) -> None:
+        super().setText(text)
+        self.setDisabled(False)
+
+    def setWhitelist(self, whitelist: Optional[str]) -> None:
+        'Restrict the text area to only accepting characters in the given string.'
+        self._charWhitelist = whitelist
+        self._lastGoodText = None
+        self._clearInvalidChars()
+
+    def _clearInvalidChars(self) -> None:
+        # Everything is valid without a whitelist.
+        if self._charWhitelist is None:
+            return
+
+        # Restore text box to last known good state when an invalid character is entered.
+        curText = self.toPlainText()
+        badChar = next((char for char in curText if char not in self._charWhitelist), None)
+        if badChar is not None:
+            if self._lastGoodText is not None:
+                self.setText(self._lastGoodText)
+                # TODO can we set the cursor to the end of the line?
+            self.validationFailure.emit(f'Disallowed character entered: {badChar}')
+        else:
+            self._lastGoodText = curText
